7868edf77588f88141e1410a434a89d31778bc37
zhangxinrui <1622360372@qq.com>
2022-07-15 10:08:45 +0800

    login code add

---
+++ src/views/goodsManage/child/sonTest.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    sonValue:{{ sonValue }}
+    <!-- <input type="text" v-model="sonValue" /> -->
+    <!-- <button @click="sonValue += 'test'">测试value</button> -->
+    <br />
+    sonNum:{{ num }}
+    <button @click="num = num + 1">测试num</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "sonTest",
+  props: {
+    sonValue: {
+      type: String,
+    },
+    num: {
+      type: Number,
+      required: true,
+    },
+  },
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style scoped>
+</style>
